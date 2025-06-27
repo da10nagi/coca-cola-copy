7ef695e08d1ccb3c410123db0f082348c0628ab2
da10nagi <meiji.dai1math@gmail.com>
2025-06-27 10:22:16 +0900

    メイン画像まで

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -4,7 +4,7 @@ export default function Page() {
 	return (
 		<div>
 			
-			<header className="flex space-x-6 p-5">
+			<header className="flex space-x-10 p-7 font-bold">
 				
 				{/*ヘッダー*/}
 				<Image src='/images/coke-company-logo-1.svg' alt='会社ロゴ' width={150} height={150} />
@@ -12,16 +12,17 @@ export default function Page() {
 				<div>サステナビリティ</div>
 			</header>
 
-			<section className="bg-black/10 p-5">
+			<section className="bg-black/5 p-5">
 				
-				{/*次やること：画像ファイルを要素でまとめてサイズ変更できるようにする！！*/}
 
 				{/*画像ファイル*/}
-				<div className="relative flex justify-center">
-					<Image src='/images/main2.jpg' alt='メインの写真' width={800} height={500} className="w-full max-w-7xl h-full max-h-6xl object-contain rounded-3xl" />
+				<div className="relative flex justify-center p-6">
+					<div className="relative w-full max-w-7xl h-[650px] overflow-hidden rounded-3xl" >
+						<Image src='/images/main2.jpg' alt='メインの写真' width={800} height={500} className="h-full w-full object-cover"/>
 					
-					{/*オーバーレイ*/}
-					<div className="absolute w-full max-w-7xl h-full max-h-8xl bg-gradient-to-l from-black/10 to-black/60  rounded-3xl"></div>
+						{/*オーバーレイ*/}
+						<div className="absolute inset-0 w-full h-full bg-gradient-to-l from-black/10 to-black/60  rounded-3xl"></div>
+					</div>
 					
 					{/*画像の中のテキスト*/}
 					<div className="absolute left-10 top-1/2 transform -translate-y-1/2 text-white m-20">
@@ -32,10 +33,10 @@ export default function Page() {
 					</div>
 				</div>
 				
-				
-				<div className="text-center p-5">
+				{/*テキスト1*/}
+				<div className="p-5 w-full text-center">
 					<h1 className="text-3xl font-bold p-5">Refresh the Wold.Make a Difference.</h1>
-					<div className="max-w-full">私たちは、世界中で愛されるブランドや、丹精込めて作り上げている様々な飲料を通じ、心身ともに人々をうるおし、さわやかさを提供してまいります。より明るい未来を築くべく、持続可能なビジネスの実現を通じ、あらゆる人々の生活、地域社会、そして地球にとって前向きな変化をもたらすことを目指します。</div>
+					<div>私たちは、世界中で愛されるブランドや、丹精込めて作り上げている様々な飲料を通じ、心身ともに人々をうるおし、さわやかさを提供してまいります。より明るい未来を築くべく、持続可能なビジネスの実現を通じ、あらゆる人々の生活、地域社会、そして地球にとって前向きな変化をもたらすことを目指します。</div>
 				</div>
 			</section>
 		</div>
